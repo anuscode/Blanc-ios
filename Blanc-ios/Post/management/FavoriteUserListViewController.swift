2e0138b47a4f3aa15c47996e6cd7a90a79f0d762
anuscode <dldyddn0624@gmail.com>
2021-02-25 04:31:25 +0900

    top inset 15 to 10

--- Blanc-ios/Post/management/FavoriteUserListViewController.swift
+++ Blanc-ios/Post/management/FavoriteUserListViewController.swift
@@ -24,7 +24,7 @@ class FavoriteUserListViewController: UIViewController {
                 forCellReuseIdentifier: SmallUserProfileTableViewCell.identifier)
         tableView.allowsSelection = false
         tableView.separatorColor = .clear
-        tableView.contentInset = UIEdgeInsets(top: 15, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
         tableView.delegate = self
         return tableView
     }()
