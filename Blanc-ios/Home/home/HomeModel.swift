18a1161a6452480f824162d1abbdaef1ae9e18bc
anuscode <dldyddn0624@gmail.com>
2021-03-05 11:32:34 +0900

    Location service removed.

--- Blanc-ios/Home/home/HomeModel.swift
+++ Blanc-ios/Home/home/HomeModel.swift
@@ -1,7 +1,7 @@
 import Foundation
 import RxSwift
 import FirebaseAuth
-
+import CoreLocation
 
 class HomeUserData {
     var recommendedUsers: [UserDTO] = []
@@ -25,14 +25,14 @@ class HomeModel {
 
     private var requestService: RequestService
 
-    init(session: Session,
-         userService: UserService,
-         requestService: RequestService) {
+    private var manager: CLLocationManager = CLLocationManager()
 
+    init(session: Session, userService: UserService, requestService: RequestService) {
         self.session = session
         self.userService = userService
         self.requestService = requestService
-        populate()
+        // populate()
+        subscribeLocationAuthorizationChanges()
     }
 
     func publish() {
@@ -45,56 +45,59 @@ class HomeModel {
 
     private func populate() {
         updateUserLocation()
-                .observeOn(SerialDispatchQueueScheduler(qos: .default))
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .flatMap { [unowned self] it -> Single<[UserDTO]> in
-                    listRecommendedUsers()
-                }
-                .do(afterSuccess: { [unowned self] users in
-                    // loop to calculate and set a distance from current user.
-                    users.distance(session)
-                    data.recommendedUsers = users
-                })
-                .flatMap { [unowned self] it -> Single<[UserDTO]> in
-                    listCloseUsers()
-                }
-                .do(afterSuccess: { [unowned self] users in
-                    // loop to calculate and set a distance from current user.
-                    users.distance(session)
-                    data.closeUsers = users
-                })
-                .flatMap { [unowned self] it -> Single<[UserDTO]> in
-                    listRealTimeAccessUsers()
-                }
-                .do(afterSuccess: { [unowned self] users in
-                    // loop to calculate and set a distance from current user.
-                    users.distance(session)
-                    data.realTimeUsers = users
-                })
-                .subscribe(onSuccess: { [unowned self] _ in
-                    publish()
-                }, onError: { err in
-                    log.error(err)
-                })
-                .disposed(by: disposeBag)
+            .observeOn(SerialDispatchQueueScheduler(qos: .default))
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .flatMap { [unowned self] it -> Single<[UserDTO]> in
+                listRecommendedUsers()
+            }
+            .do(afterSuccess: { [unowned self] users in
+                // loop to calculate and set a distance from current user.
+                users.distance(session)
+                data.recommendedUsers = users
+            })
+            .flatMap { [unowned self] it -> Single<[UserDTO]> in
+                listCloseUsers()
+            }
+            .do(afterSuccess: { [unowned self] users in
+                // loop to calculate and set a distance from current user.
+                users.distance(session)
+                data.closeUsers = users
+            })
+            .flatMap { [unowned self] it -> Single<[UserDTO]> in
+                listRealTimeAccessUsers()
+            }
+            .do(afterSuccess: { [unowned self] users in
+                // loop to calculate and set a distance from current user.
+                users.distance(session)
+                data.realTimeUsers = users
+            })
+            .subscribe(onSuccess: { [unowned self] _ in
+                publish()
+            }, onError: { err in
+                log.error(err)
+            })
+            .disposed(by: disposeBag)
     }
 
     private func listRecommendedUsers() -> Single<[UserDTO]> {
-        userService.listRecommendedUsers(uid: auth.uid, userId: session.id)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(MainScheduler.instance)
+        userService
+            .listRecommendedUsers(uid: auth.uid, userId: session.id)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(MainScheduler.instance)
     }
 
     private func listCloseUsers() -> Single<[UserDTO]> {
-        userService.listCloseUsers(uid: auth.uid, userId: session.id)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(MainScheduler.instance)
+        userService
+            .listCloseUsers(uid: auth.uid, userId: session.id)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(MainScheduler.instance)
     }
 
     private func listRealTimeAccessUsers() -> Single<[UserDTO]> {
-        userService.listRealTimeAccessUsers(uid: auth.uid, userId: session.id)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(MainScheduler.instance)
+        userService
+            .listRealTimeAccessUsers(uid: auth.uid, userId: session.id)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(MainScheduler.instance)
     }
 
     private func updateUserLocation() -> Single<Location> {
@@ -102,28 +105,59 @@ class HomeModel {
         let userId: String? = session.id
         var coord: Coordinate?
         var addr: String?
-        return LocationService.shared.getCurrentLocation()
-                .do(onSuccess: { coordinate in
-                    coord = coordinate
-                })
-                .flatMap({ _ -> Single<String> in
-                    LocationService.shared.getAddress(by: coord)
-                })
-                .do(onSuccess: { address in
-                    addr = address
-                })
-                .flatMap({ _ -> Single<Location> in
-                    self.userService.updateUserLocation(
-                            uid: uid,
-                            userId: userId,
-                            latitude: coord?.latitude ?? 0,
-                            longitude: coord?.longitude ?? 0,
-                            area: addr ?? "알 수 없음"
-                    )
-                })
-                .do(onSuccess: { location in
-                    self.session.user?.location = location
-                })
+        return manager.rx
+            .location
+            .subscribeOn(MainScheduler.instance)
+            .do(onNext: { location in
+                coord = Coordinate(location)
+            })
+            .observeOn(MainScheduler.instance)
+            .flatMap({ _ -> Observable<String> in
+                if (self.manager.authorizationStatus.rawValue <= 2) {
+                    return Observable.of("알 수 없음")
+                } else {
+                    return self.manager.rx.placemark.map({ placemark in
+                        (placemark.locality ?? "알 수 없음")
+                    })
+                }
+            })
+            .do(onNext: { locality in
+                addr = locality
+            })
+            .observeOn(SerialDispatchQueueScheduler(qos: .default))
+            .flatMap({ _ -> Single<Location> in
+                self.userService.updateUserLocation(
+                    uid: uid,
+                    userId: userId,
+                    latitude: coord?.latitude ?? 0,
+                    longitude: coord?.longitude ?? 0,
+                    area: addr ?? "알 수 없음"
+                )
+            })
+            .do(onNext: { location in
+                self.session.user?.location = location
+            })
+            .take(1)
+            .asSingle()
+    }
+
+    private func subscribeLocationAuthorizationChanges() {
+        manager.rx
+            .didChangeAuthorization
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { _, status in
+                switch status {
+                case .denied, .notDetermined, .restricted:
+                    self.manager.requestAlwaysAuthorization()
+                default:
+                    log.info("Currently the location authorization is well granted..")
+                }
+                self.populate()
+            }, onError: { err in
+                log.error(err)
+            })
+            .disposed(by: disposeBag)
     }
 
     func request(_ user: UserDTO?,
@@ -142,34 +176,35 @@ class HomeModel {
         }
 
         requestService.createRequest(
-                        currentUser: auth.currentUser!, uid: auth.uid,
-                        userId: user?.id, requestType: RequestType.FRIEND)
-                .do(onSuccess: { request in onComplete(request) })
-                .flatMap({ [unowned self] _ in session.refresh() })
-                .subscribe(onSuccess: { [unowned self] _ in
-                    animationDone.subscribe({ _ in
-                        remove(user)
-                    }).disposed(by: disposeBag)
-                }, onError: { err in
-                    log.error(err)
-                    onError("친구신청 도중 에러가 발생 하였습니다.")
+                currentUser: auth.currentUser!, uid: auth.uid,
+                userId: user?.id, requestType: RequestType.FRIEND)
+            .do(onSuccess: { request in onComplete(request) })
+            .flatMap({ [unowned self] _ in session.refresh() })
+            .subscribe(onSuccess: { [unowned self] _ in
+                animationDone.subscribe({ _ in
+                    remove(user)
                 }).disposed(by: disposeBag)
+            }, onError: { err in
+                log.error(err)
+                onError("친구신청 도중 에러가 발생 하였습니다.")
+            })
+            .disposed(by: disposeBag)
     }
 
     func poke(_ user: UserDTO?, completion: @escaping (_ message: String) -> Void) {
         userService.pushPoke(uid: auth.uid, userId: user?.id)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(SerialDispatchQueueScheduler(qos: .default))
-                .subscribe(onSuccess: { [unowned self] data in
-                    DispatchQueue.main.async {
-                        RealmService.setPokeHistory(uid: session.uid, userId: user?.id)
-                    }
-                    completion("상대방 옆구리를 찔렀습니다.")
-                }, onError: { err in
-                    log.error(err)
-                    completion("찔러보기 도중 에러가 발생 하였습니다.")
-                })
-                .disposed(by: disposeBag)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(SerialDispatchQueueScheduler(qos: .default))
+            .subscribe(onSuccess: { [unowned self] data in
+                DispatchQueue.main.async {
+                    RealmService.setPokeHistory(uid: session.uid, userId: user?.id)
+                }
+                completion("상대방 옆구리를 찔렀습니다.")
+            }, onError: { err in
+                log.error(err)
+                completion("찔러보기 도중 에러가 발생 하였습니다.")
+            })
+            .disposed(by: disposeBag)
     }
 
     func rate(_ user: UserDTO?, _ score: Int, onSuccess: @escaping () -> Void, onError: @escaping (_ message: String) -> Void) {
@@ -177,19 +212,19 @@ class HomeModel {
             return
         }
         userService.updateUserStarRatingScore(uid: auth.uid, userId: user!.id, score: score)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(MainScheduler.instance)
-                .subscribe(onSuccess: { _ in
-                    self.session.user?.starRatingsIRated?.append(StarRating(userId: user!.id!, score: score))
-                    self.session.publish()
-                    self.publish()
-                    onSuccess()
-                    log.info("Successfully rated score \(score) at user: \(user!.id ?? "??")")
-                }, onError: { err in
-                    log.error(err)
-                    onError("평가 도중 에러가 발생 하였습니다.")
-                })
-                .disposed(by: disposeBag)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(MainScheduler.instance)
+            .subscribe(onSuccess: { _ in
+                self.session.user?.starRatingsIRated?.append(StarRating(userId: user!.id!, score: score))
+                self.session.publish()
+                self.publish()
+                onSuccess()
+                log.info("Successfully rated score \(score) at user: \(user!.id ?? "??")")
+            }, onError: { err in
+                log.error(err)
+                onError("평가 도중 에러가 발생 하였습니다.")
+            })
+            .disposed(by: disposeBag)
     }
 
     func getStarRatingIRated(_ userId: String?) -> StarRating? {
@@ -213,22 +248,21 @@ class HomeModel {
     }
 
     func updateUserLastLoginAt() {
-
         let current = Int(NSDate().timeIntervalSince1970)
         let lastLoginAt = session.user?.lastLoginAt ?? 0
         let delta = current - lastLoginAt
-        if (delta < 60) {
+        if (delta < 120) {
             return
         }
-
-        userService.updateUserLastLoginAt(uid: auth.uid, userId: session.id)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(SerialDispatchQueueScheduler(qos: .default))
-                .subscribe(onSuccess: { _ in
-                    self.session.user?.lastLoginAt = Int(NSDate().timeIntervalSince1970)
-                }, onError: { err in
-                    log.error(err)
-                })
-                .disposed(by: disposeBag)
+        userService
+            .updateUserLastLoginAt(uid: auth.uid, userId: session.id)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(SerialDispatchQueueScheduler(qos: .default))
+            .subscribe(onSuccess: { _ in
+                self.session.user?.lastLoginAt = Int(NSDate().timeIntervalSince1970)
+            }, onError: { err in
+                log.error(err)
+            })
+            .disposed(by: disposeBag)
     }
 }
