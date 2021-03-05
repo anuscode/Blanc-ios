0029388a830ff8aef13f9945f5d6e3fca7ab20d3
anuscode <dldyddn0624@gmail.com>
2021-03-05 11:35:20 +0900

    Conversation service indentation.

--- Blanc-ios/Dependencies/ConversationService.swift
+++ Blanc-ios/Dependencies/ConversationService.swift
@@ -19,58 +19,61 @@ class ConversationService {
 
     // GET
     func getConversation(uid: String?, conversationId: String?) -> Single<ConversationDTO> {
-        provider.rx.request(.getConversation(uid: uid, conversationId: conversationId))
-                .debug()
-                .filterSuccessfulStatusAndRedirectCodes()
-                .map(ConversationDTO.self, using: decoder)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+        provider.rx
+            .request(.getConversation(uid: uid, conversationId: conversationId))
+            .debug()
+            .filterSuccessfulStatusAndRedirectCodes()
+            .map(ConversationDTO.self, using: decoder)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
     }
 
     func listUserConversations(uid: String?) -> Single<[ConversationDTO]> {
-        provider.rx.request(.listUserConversations(uid: uid))
-                .debug()
-                .filterSuccessfulStatusAndRedirectCodes()
-                .map([ConversationDTO].self, using: decoder)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+        provider.rx
+            .request(.listUserConversations(uid: uid))
+            .debug()
+            .filterSuccessfulStatusAndRedirectCodes()
+            .map([ConversationDTO].self, using: decoder)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
     }
 
     // POST
     func sendMessage(uid: String?, conversationId: String?, message: String?) -> Single<MessageDTO> {
-        provider.rx.request(.sendMessage(uid: uid, conversationId: conversationId, message: message))
-                .debug()
-                .filterSuccessfulStatusAndRedirectCodes()
-                .map(MessageDTO.self, using: decoder)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+        provider.rx
+            .request(.sendMessage(uid: uid, conversationId: conversationId, message: message))
+            .debug()
+            .filterSuccessfulStatusAndRedirectCodes()
+            .map(MessageDTO.self, using: decoder)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
     }
 
     // PUT
     func updateConversationAvailable(currentUser: User,
                                      uid: String?,
                                      conversationId: String?) -> Single<Void> {
-        currentUser.rx.getIDTokenResult()
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .flatMap { [unowned self] result in
-                    provider.rx.request(.updateConversationAvailable(
-                                    idToken: result.token,
-                                    uid: uid,
-                                    conversationId: conversationId,
-                                    available: true))
-                            .debug()
-                            .filterSuccessfulStatusAndRedirectCodes()
-                            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                            .map { _ in
-                                Void()
-                            }
-                }
-                .asSingle()
+        currentUser.rx
+            .getIDTokenResult()
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .flatMap { [unowned self] result in
+                provider.rx.request(.updateConversationAvailable(
+                        idToken: result.token,
+                        uid: uid,
+                        conversationId: conversationId,
+                        available: true))
+                    .debug()
+                    .filterSuccessfulStatusAndRedirectCodes()
+                    .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+                    .map({ _ in Void() })
+            }
+            .asSingle()
     }
 
     // DELETE
     func leaveConversation(uid: String?, conversationId: String?, userId: String?) -> Single<Void> {
-        provider.rx.request(.leaveConversation(uid: uid, conversationId: conversationId, userId: userId))
-                .debug()
-                .filterSuccessfulStatusAndRedirectCodes()
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .map({ _ in Void() })
+        provider.rx
+            .request(.leaveConversation(uid: uid, conversationId: conversationId, userId: userId))
+            .debug()
+            .filterSuccessfulStatusAndRedirectCodes()
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .map({ _ in Void() })
     }
 }
