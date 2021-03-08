9495572032cab19e883b2c0bbd4def4bc68e6b50
anuscode <dldyddn0624@gmail.com>
2021-03-08 18:07:20 +0900

    requests model updated.

--- Blanc-ios/Request/requests/RequestsModel.swift
+++ Blanc-ios/Request/requests/RequestsModel.swift
@@ -30,100 +30,124 @@ class RequestsModel {
     }
 
     func populate() {
-        requestService.listRequests(uid: session.uid)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(SerialDispatchQueueScheduler(qos: .default))
-                .subscribe(onSuccess: { [unowned self] requests in
-                    self.requests = requests
-                    publish()
-                }, onError: { err in
-                    log.error(err)
-                })
-                .disposed(by: disposeBag)
+        requestService
+            .listRequests(uid: session.uid)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(SerialDispatchQueueScheduler(qos: .default))
+            .subscribe(onSuccess: { [unowned self] requests in
+                self.requests = requests
+                publish()
+            }, onError: { err in
+                log.error(err)
+            })
+            .disposed(by: disposeBag)
     }
 
     private func subscribeBroadcast() {
-        Broadcast.observe()
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(SerialDispatchQueueScheduler(qos: .default))
-                .subscribe(onNext: { push in
-                    if (push.isRequest()) {
-                        self.insertRequest(requestId: push.requestId)
+        Broadcast
+            .observe()
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(SerialDispatchQueueScheduler(qos: .default))
+            .subscribe(onNext: { push in
+                if (push.isRequest()) {
+                    self.insertRequest(requestId: push.requestId)
+                }
+                if (push.isMatched()) {
+                    if let request = self.requests.first(where: { $0.id == push.requestId }) {
+                        let index = self.requests.firstIndex(of: request)
+                        self.requests.remove(at: index!)
                     }
-                    if (push.isMatched()) {
-                        if let request = self.requests.first(where: { $0.id == push.requestId }) {
-                            let index = self.requests.firstIndex(of: request)
-                            self.requests.remove(at: index!)
-                        }
 
-                        if let requestId = push.requestId {
-                            self.session.user?.userIdsMatched?.append(requestId)
-                        }
+                    if let requestId = push.requestId {
+                        self.session.user?.userIdsMatched?.append(requestId)
                     }
-                }, onError: { err in
-                    log.error(err)
-                })
-                .disposed(by: disposeBag)
+                }
+            }, onError: { err in
+                log.error(err)
+            })
+            .disposed(by: disposeBag)
     }
 
     private func subscribeBackground() {
-        Background.observe()
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(SerialDispatchQueueScheduler(qos: .default))
-                .subscribe(onNext: { push in
-                    self.populate()
-                }, onError: { err in
-                    log.error(err)
-                })
-                .disposed(by: disposeBag)
+        Background
+            .observe()
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(SerialDispatchQueueScheduler(qos: .default))
+            .subscribe(onNext: { push in
+                self.populate()
+            }, onError: { err in
+                log.error(err)
+            })
+            .disposed(by: disposeBag)
     }
 
     private func insertRequest(requestId: String?) {
-        requestService.getRequest(uid: session.uid, requestId: requestId)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(SerialDispatchQueueScheduler(qos: .default))
-                .subscribe(onSuccess: { request in
-                    self.requests.insert(request, at: 0)
-                    if let userId = request.userFrom?.id {
-                        self.session.user?.userIdsSentMeRequest?.append(userId)
-                    }
-                    self.publish()
-                }, onError: { err in
-                    log.error(err)
-                })
-                .disposed(by: disposeBag)
+        requestService
+            .getRequest(uid: session.uid, requestId: requestId)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(SerialDispatchQueueScheduler(qos: .default))
+            .subscribe(onSuccess: { request in
+                self.requests.insert(request, at: 0)
+                if let userId = request.userFrom?.id {
+                    self.session.user?.userIdsSentMeRequest?.append(userId)
+                }
+                self.publish()
+            }, onError: { err in
+                log.error(err)
+            })
+            .disposed(by: disposeBag)
     }
 
     func accept(request: RequestDTO?, onSuccess: @escaping () -> Void, onError: @escaping () -> Void) {
-        requestService.updateRequest(uid: session.uid, requestId: request?.id, response: Response.ACCEPTED)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(SerialDispatchQueueScheduler(qos: .default))
-                .flatMap({ _ -> Single<UserDTO> in self.session.refresh() })
-                .subscribe(onSuccess: { _ in
-                    if let index = self.requests.firstIndex(of: request!) {
-                        self.requests.remove(at: index)
-                        self.publish()
-                    }
-                    onSuccess() // refresh conversations.
-                }, onError: { err in
-                    onError()
-                })
-                .disposed(by: disposeBag)
+
+        guard let uid = session.uid,
+              let requestId = request?.id else {
+            return
+        }
+
+        requestService
+            .updateRequest(uid: uid, requestId: requestId, response: .ACCEPTED)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(SerialDispatchQueueScheduler(qos: .default))
+            .flatMap({ _ -> Single<UserDTO> in
+                self.session.refresh()
+            })
+            .observeOn(MainScheduler.asyncInstance)
+            .subscribe(onSuccess: { _ in
+                if let index = self.requests.firstIndex(where: {
+                    $0.id == requestId
+                }) {
+                    self.requests.remove(at: index)
+                }
+                self.publish()
+                onSuccess()
+            }, onError: { err in
+                onError()
+            })
+            .disposed(by: disposeBag)
     }
 
     func decline(request: RequestDTO?, onError: @escaping () -> Void) {
-        requestService.updateRequest(uid: session.uid, requestId: request?.id, response: Response.DECLINED)
-                .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
-                .observeOn(SerialDispatchQueueScheduler(qos: .default))
-                .subscribe(onSuccess: { [unowned self] in
-                    let index = requests.firstIndex(of: request!)
-                    if (index != nil) {
-                        requests.remove(at: index!)
-                        publish()
-                    }
-                }, onError: { err in
-                    onError()
-                })
-                .disposed(by: disposeBag)
+
+        guard let uid = session.uid,
+              let requestId = request?.id else {
+            return
+        }
+
+        requestService
+            .updateRequest(uid: uid, requestId: requestId, response: .DECLINED)
+            .subscribeOn(SerialDispatchQueueScheduler(qos: .default))
+            .observeOn(SerialDispatchQueueScheduler(qos: .default))
+            .subscribe(onSuccess: { _ in
+                if let index = self.requests.firstIndex(where: {
+                    $0.id == requestId
+                }) {
+                    self.requests.remove(at: index)
+                }
+                self.publish()
+            }, onError: { err in
+                onError()
+            })
+            .disposed(by: disposeBag)
     }
 }
